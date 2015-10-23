9fae534e3723455af628319421bde3da51b43979
Mike Mathew <m2mathew@me.com>
2015-10-22 20:36:22 -0500

    Styled the form element with some beautiful animations

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,31 @@
 'use strict';
+
+var $ = require('jquery');
+
+$(document).ready(function () {
+	console.log('ready');
+
+	var $password = $('#password');
+	var $err = $('#error');
+	var $output = $('.output');
+
+	$('input').on('keyup', function() {
+		var test = $password.val();
+
+		// if(test )
+
+		// return 0 1 2 3
+	});
+
+	$('form').on('submit' ,function(e) {
+		e.preventDefault();
+
+
+
+	});
+
+
+
+
+
+});
