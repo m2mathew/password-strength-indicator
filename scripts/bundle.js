c995ab94d22df8b17fa43b570f37e42bb469d676
Mike Mathew <m2mathew@me.com>
2015-10-25 22:31:35 -0500

    As password is being entered, the user is shown the password strength

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -9216,22 +9216,51 @@ return jQuery;
 var $ = require('jquery');
 
 $(document).ready(function () {
-	console.log('ready');
 
 	var $password = $('#password');
 	var $err = $('#error');
-	var $output = $('.output');
+	var $output = $('#output');
+
+	var numCount = 0;
+	var letterCount = 0;
+	var charCount = 0;
 
 	$('input').on('keyup', function () {
+
 		var test = $password.val();
+		$output.text('');
+		$err.text('');
 
-		// if(test )
+		for (var i = 0; i < test.length; i++) {
 
-		// return 0 1 2 3
-	});
+			if (test.charCodeAt(i) >= 48 && test.charCodeAt(i) <= 57) {
+				numCount++;
+			}
 
-	$('form').on('submit', function (e) {
-		e.preventDefault();
+			if (test.charCodeAt(i) >= 65 && test.charCodeAt(i) <= 90 || test.charCodeAt(i) >= 97 && test.charCodeAt(i) <= 122) {
+				letterCount++;
+			}
+
+			if (test.charCodeAt(i) >= 33 && test.charCodeAt(i) <= 47 || test.charCodeAt(i) >= 58 && test.charCodeAt(i) <= 64 || test.charCodeAt(i) >= 91 && test.charCodeAt(i) <= 96) {
+				charCount++;
+			}
+		}
+
+		if (numCount === test.length && test.length > 0) {
+			$output.text('very weak password');
+		}
+
+		if (letterCount === test.length && test.length > 0) {
+			$output.text('weak password');
+		}
+
+		if (test.length >= 8 && numCount > 0 && letterCount > 0) {
+			$output.text('strong password');
+		}
+
+		if (test.length >= 8 && numCount > 0 && letterCount > 0 && charCount > 0) {
+			$output.text('very strong password');
+		}
 	});
 });
 
